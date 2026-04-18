4004d17c7fefc979eb0ed4ae20fac5e02f295401
Test <tast@test.ru>
2026-04-18 09:55:45 +0300

    first commit

---
+++ test.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>практическая работа</title>
+</head>
+<body>
+    <h3>1-1 задание</h3>
+    <?php
+        $celoe = 52;            
+        $s_tchk = 6.99;        
+        $stroka = "мяу мяу"; 
+        $logich = true;      
+        $massive = [1, 2, 3];    
+
+        echo "тип \$celoe: " . gettype($celoe) . "<br>\n";
+        echo "тип \$s_tchk: " . gettype($s_tchk) . "<br>\n";
+        echo "тип \$stroka: " . gettype($stroka) . "<br>\n";
+        echo "тип \$logich: " . gettype($logich) . "<br>\n";
+        echo "тип \$massive: " . gettype($massive) . "<br>\n";
+    ?>
+    </body>
+</html>
